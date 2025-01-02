aeb4f508a83966cc7e59834db979845b57cc8583
awilsofl <apreswilson@gmail.com>
2025-01-02 09:33:27 -0500

    test whitespace

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,18 +1,18 @@
 export default function Navbar() {
-	return (
-		<nav className='flex flex-col justify-between w-5/6 gap-4 p-4 m-auto place-items-center md:flex-row md:w-3/6'>
-			<h1 className='text-2xl md:text-base'>Company Name</h1>
-			<ul className='flex gap-6'>
-				<li className='transition-colors cursor-pointer hover:text-red-500'>
-					Home
-				</li>
-				<li className='transition-colors cursor-pointer hover:text-red-500'>
-					About
-				</li>
-				<li className='transition-colors cursor-pointer hover:text-red-500'>
-					Contact
-				</li>
-			</ul>
-		</nav>
-	);
+   return (
+      <nav className='flex flex-col justify-between w-5/6 gap-4 p-4 m-auto place-items-center md:flex-row md:w-3/6'>
+         <h1 className='text-2xl md:text-base'>Company Name</h1>
+         <ul className='flex gap-6'>
+            <li className='transition-colors cursor-pointer hover:text-red-500'>
+               Home
+            </li>
+            <li className='transition-colors cursor-pointer hover:text-red-500'>
+               About
+            </li>
+            <li className='transition-colors cursor-pointer hover:text-red-500'>
+               Contact
+            </li>
+         </ul>
+      </nav>
+   );
 }
